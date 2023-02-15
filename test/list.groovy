119b2f82d6fa2528748a7c753c03bdb55448c157
James Gillen <jamgill700@gmail.com>
2023-02-15 21:37:47 +0000

    test

---
+++ test/list.groovy
@@ -0,0 +1,16 @@
+// def list = ["Apple", "Box"]
+
+// list.add("house")
+
+// print(list)
+
+// String new_tenant_alias = "house"
+
+// if(!new_tenant_alias){
+//     print("empty")
+// }
+// else{
+//     print(new_tenant_alias)
+// }
+
+print("This is the first stage")
